dc984ee50fbf58e369ef47831cc6576a495f252e
Viktor Meduneckij <viktor@dappradar.com>
2025-12-22 16:05:10 +0200

    feature: add seo data

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,3 +1,16 @@
+<script setup lang="ts">
+useSeoMeta({
+  title: "Viktor Meduneckij | Frontend developer",
+  ogTitle: "Viktor Meduneckij | Frontend developer",
+  description:
+    "Lithuanian frontend developer with nearly 10 years of experience. I focus on building maintainable, high-performance frontend for web.",
+  ogDescription:
+    "Lithuanian frontend developer with nearly 10 years of experience. I focus on building maintainable, high-performance frontend for web.",
+  ogImage: "https://www.meduneckij.lt/assets/og-image.png",
+  twitterCard: "summary_large_image",
+});
+</script>
+
 <template>
   <section>
     <Hero />
